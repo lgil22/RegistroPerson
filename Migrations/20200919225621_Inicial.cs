e2e4d6aeaef6c68b0df96a71afd9fde048124bb5
Luis David Gil Baez <luis_gil@ucne.edu.do>
2020-09-19 18:56:55 -0400

    Añadiendo migracion

---
+++ Migrations/20200919225621_Inicial.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RegistroPerson.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Personas",
+                columns: table => new
+                {
+                    personaId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    nombre = table.Column<string>(maxLength: 75, nullable: false),
+                    telefono = table.Column<string>(maxLength: 10, nullable: false),
+                    cedula = table.Column<string>(nullable: false),
+                    direccion = table.Column<string>(maxLength: 80, nullable: false),
+                    fecha = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Personas", x => x.personaId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Personas");
+        }
+    }
+}
